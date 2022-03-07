fa7ddde12f618b1c36b27f2771e8c0b1418da279
MaxHuzarevich <pinsk.maks@mail.ru>
2022-03-07 23:48:04 +0300

    add styles Cart.styles.ts

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export type CartItemType = {
     image: string;
     price: string;
     title: string;
-    amount: string;
+    amount: number;
 }
 
 const getProducts = async (): Promise<CartItemType[]> =>
@@ -26,25 +26,30 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 const App = () => {
 
-    const [cartOpen, setCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([] as CartItemType[])
-    const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+    const [cartOpen, setCartOpen] = useState(false),
+        [cartItems, setCartItems] = useState([] as CartItemType[]),
+        {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts),
 
-    const getTotalItems = () => null;
+        getTotalItems = (items: CartItemType[]) =>
+            items.reduce((ack, item) => ack + item.amount, 0),
 
-    const handleAddToCart = (clickedItem: CartItemType) => null;
+        handleAddToCart = (clickedItem: CartItemType) => null,
 
-    const handleRemoveFromCart = () => null;
+        handleRemoveFromCart = () => null;
 
     if (isLoading) return <LinearProgress/>
     if (error) return <div>Something went wrong ...</div>
 
     return (
         <Wrapper>
-            <Drawer anchor='right' open={cartOpen} onClose={()=>setCartOpen(false)}>
+            <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
                 Cart goes here
             </Drawer>
-            <StyledButton onClick={() => setCartOpen(true)}/>
+            <StyledButton onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+                    <AddShoppingCartIcon/>
+                </Badge>
+            </StyledButton>
             <Grid container spacing={3}>
                 {data?.map(item => (
                     <Grid item key={item.id} xs={12} sm={4}>
